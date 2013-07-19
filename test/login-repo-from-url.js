c103e3f39c34d3d5492a072352e58045e816b1f8
Thorsten Lorenz <thlorenz@gmx.de>
2013-07-18 22:53:27 -0400

    testing login and repo from url extraction

---
+++ test/login-repo-from-url.js
@@ -0,0 +1,17 @@
+'use strict';
+/*jshint asi: true */
+
+var test = require('tap').test
+var loginRepo = require('../lib/login-repo-from-url')
+
+test('\ndetermines the user login and repo name correcctly from the url', function (t) {
+
+  t.deepEqual({ login: 'component', repo: 'throttle'}, loginRepo('git://github.com/component/throttle.git'))
+  t.deepEqual({ login: 'component', repo: 'throttle'}, loginRepo('https://github.com/component/throttle.git'))
+  t.deepEqual({ login: 'substack', repo: 'node-browserify'}, loginRepo('https://github.com/substack/node-browserify.git'))
+  t.deepEqual({ login: 'substack', repo: 'node-browserify'}, loginRepo('https://github.com/substack/node-browserify'))
+  t.deepEqual({ login: 'thlorenz', repo: 'runnel'}, loginRepo('github.com/thlorenz/runnel'))
+  t.deepEqual({ login: 'thlorenz', repo: 'runnel'}, loginRepo('thlorenz/runnel'))
+
+  t.end()
+})
