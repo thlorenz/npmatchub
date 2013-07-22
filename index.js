214c2ea274bd1b06b53078358665d8310b75ee78
Thorsten Lorenz <thlorenz@gmx.de>
2013-07-21 20:42:53 -0400

    exposing repos stabs function

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 'use strict';
 
 // main functionality
-exports.logins            =  require('./lib/logins-count');
-exports.repos             =  require('./lib/repos');
+exports.logins = require('./lib/logins-count');
+exports.repos = require('./lib/repos');
 
 // exposed to allow calling the original when overriding 'resolve' for repos
 exports.resolve = require('./lib/repos-resolve-github');
 
 // exposed utilities
 exports.loginsPercent = require('./lib/logins-percent');
+exports.stabs = require('./lib/repos-stabs');
